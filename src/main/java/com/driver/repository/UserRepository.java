fa24920554cb5acaad83eb8d62633da4f585124c
unknown <visapurkar1998@gmail.com>
2023-05-05 18:51:14 +0530

    removed implementation and added autowired

--- src/main/java/com/driver/repository/UserRepository.java
+++ src/main/java/com/driver/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.driver.repository;
 
 
 import com.driver.model.User;
+import com.driver.model.WebSeries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
